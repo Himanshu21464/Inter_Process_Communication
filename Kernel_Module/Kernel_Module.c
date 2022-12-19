37e137c3d34a0b15be1dc6cdebc4a57b5c43022d
Himanshu <98085386+Himanshu21464@users.noreply.github.com>
2022-12-19 18:05:22 +0530

    Add files via upload

---
+++ Kernel_Module/Kernel_Module.c
@@ -0,0 +1,35 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched/signal.h>
+#include<linux/string.h>
+
+char *process_name="systemd";
+
+module_param(process_name,charp,S_IRUGO);
+
+static int tasks_init(void){
+    struct task_struct *process;    
+    pr_info("%s: In init Function \n", __func__);
+    
+    for_each_process(process) {
+        
+        if(strcmp(process->comm,process_name)==0){
+        printk(KERN_INFO "Process ID       : %d",process->pid);
+        printk(KERN_INFO "User ID          : %d",process->cred->uid);
+        printk(KERN_INFO "Process Group ID : %d",process->cred->gid);
+        //printk(KERN_INFO "Command Path     : %s",process->cdir);
+        }else{
+            continue;
+        }
+         
+    }
+    return 0;
+}
+
+static void tasks_exit(void){
+    pr_info("%s: In exit Function\n", __func__);
+}
+
+MODULE_LICENSE("GPL");
+module_init(tasks_init);
+module_exit(tasks_exit)
