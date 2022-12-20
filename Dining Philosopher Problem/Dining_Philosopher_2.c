27f5a4f5d30b5964bf5f86942206e4389208f362
Himanshu <98085386+Himanshu21464@users.noreply.github.com>
2022-12-20 23:23:01 +0530

    Update Dining_Philosopher_2.c

--- Dining Philosopher Problem/Dining_Philosopher_2.c
+++ Dining Philosopher Problem/Dining_Philosopher_2.c
@@ -1,8 +1,8 @@
-#include <sys/types.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <time.h>
 #include <errno.h>
 #include <pthread.h>
@@ -53,23 +53,31 @@ void *PHILOSOPHER (void *arg){
     int PHILOSOPHER_NUM = *((int *) arg);
     char P_STRING [100];
     while (1) {
+        //------------------checking for Philosopher Mode---------------------- 
         bool eat = rand () % 2;	
+
+    //-----------------------------------EATING------------------------------------    
 	if (eat) {
             sprintf (P_STRING, "Philosopher %d: Will eat!\n", PHILOSOPHER_NUM+1);
             STRING (P_STRING);
+        //-------------SELECTING LEFT FORK BASED ON ITS AVAILABILITY--------------------
 	    LEFT_FORK = (PHILOSOPHER_NUM == 4) ? 0 : PHILOSOPHER_NUM;
             if ((r = pthread_mutex_lock (&fork_mutex [LEFT_FORK])) != 0) {
                 printf("error");exit(EXIT_FAILURE);
             }
+        //-------------SELECTING RIGHT FORK BASED ON ITS AVAILABILITY--------------------
 	    RIGHT_FORK = (PHILOSOPHER_NUM == 4) ? 4 : PHILOSOPHER_NUM + 1;
             if ((r = pthread_mutex_lock (&fork_mutex [RIGHT_FORK])) != 0) {
                 fprintf (stderr, "Error = %d (%s)\n", r, strerror (r)); exit (EXIT_FAILURE);
             }
             sprintf (P_STRING, "Philosopher %d: Got forks: %d and %d\n", PHILOSOPHER_NUM+1, LEFT_FORK+1, RIGHT_FORK+1);
             STRING (P_STRING);
-            sprintf (P_STRING, "Philosopher %d:  Eating....\n", PHILOSOPHER_NUM+1);
+            sprintf (P_STRING, "Philosopher %d:  Eating\n", PHILOSOPHER_NUM+1);
             STRING (P_STRING);
-            sleep (rand () % 5 + 1);
+            sleep (1);
+
+            //------------------CHECKING IF FORKS ARE FREE OR NOT --------------------------
+
             if ((r = pthread_mutex_unlock (&fork_mutex [LEFT_FORK])) != 0) {
                 printf("error");exit(EXIT_FAILURE);
             }
@@ -78,10 +86,11 @@ void *PHILOSOPHER (void *arg){
                 printf("error");exit(EXIT_FAILURE);
             }
 	}
+    //------------------------------THINKING---------------------------------
 	else { 
-            sprintf (P_STRING, "Philosopher %d: Thinking....\n", PHILOSOPHER_NUM+1);
+            sprintf (P_STRING, "Philosopher %d: Thinking\n", PHILOSOPHER_NUM+1);
             STRING (P_STRING);
-            sleep (rand () % 5 + 1);
+            sleep (1);
 	}
         sprintf (P_STRING, "Philosopher %d: Finished %s", PHILOSOPHER_NUM+1, (eat) ? "eating.\n" : "thinking.\n");
         STRING (P_STRING);
@@ -93,7 +102,7 @@ void *TEMP_FUNCTION (void *arg){
     int r;
 
     while (1) {  
-    
+        //----------------LOCK MUTEX FOR ACCESS REQUEST--------------------
         if ((r = pthread_mutex_lock (&BUFFER_MUTEX)) != 0) {
             printf("error");exit(EXIT_FAILURE);
         }
@@ -106,6 +115,8 @@ void *TEMP_FUNCTION (void *arg){
                BPI = 0;
             BUFF_LEFT++;
             pthread_cond_signal (&BUFFER_CONDITION);
+
+        //----------------UNLOCK MUTEX FOR ACCESS REQUEST--------------------
         if ((r = pthread_mutex_unlock (&BUFFER_MUTEX)) != 0) {
             printf("error");exit(EXIT_FAILURE);
         }
@@ -118,23 +129,26 @@ int main (int argc, char **argv){
     pthread_t THREAD [5], THREAD_TEMP;
     int i, r;
     BUFF_INDEX = BPI = 0;
-    time_t now = time (NULL);
-    srand ((unsigned int) (now % 937));
+
+  
     if ((r = pthread_create (&THREAD_TEMP, NULL, TEMP_FUNCTION, NULL)) != 0) {
         printf("error");exit(EXIT_FAILURE);
     }
     int THREAD_PH [5];
+      //---------------------CREATING PHILOSOPHER THREADS--------------------------
     for (i = 0; i < 5; i++) {
         THREAD_PH [i] = i;
         if ((r = pthread_create (&THREAD [i], NULL, PHILOSOPHER, (void *) &THREAD_PH [i])) != 0) {
             printf("error");exit(EXIT_FAILURE);
         }
     }
+      //---------------------JOINING PHILOSOPHER THREADS--------------------------
     for (i = 0; i < 5; i++)
         if ((r = pthread_join (THREAD [i], NULL)) == -1) {
             printf("error");exit(EXIT_FAILURE);
         }
     while (LINE) sleep (1);
+    //-----------------------IF NO MORE LINE ARE REQUIRED TO PRINT-----------------------
     if ((r = pthread_cancel (THREAD_TEMP)) != 0) {
         printf("error");exit(EXIT_FAILURE);
     }
