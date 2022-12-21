69d6b044cc2d529d2ec2b93cc966ae219f2b33f7
Himanshu <98085386+Himanshu21464@users.noreply.github.com>
2022-12-21 22:05:35 +0530

    Update Q1_A1.c

--- Dining Philosopher/Q1_A1.c
+++ Dining Philosopher/Q1_A1.c
@@ -1,72 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
+#include<unistd.h>
 
-#define N 5
+#define N 5  // Number of philosophers
 
-int ORDERING[N] = { 0, 1, 2, 3, 4 };
-pthread_mutex_t BOWLS[N];
-pthread_mutex_t FORKS[N];
-
-void* Philosopher_Function(void* num)
-{
-    int PH_ID = *(int*)num;
-    int PREVIOUS_FORK = PH_ID;
-    int NEXT_FORK = (PH_ID + 1) % N;
-
-    while (1) {
-        // Think
-        printf("Philosopher : %d is THINKING ...\n", PH_ID+1);
-        sleep(1);
-
-        // if bowl available
-        while (pthread_mutex_trylock(&BOWLS[PH_ID]) != 0) {
-            sleep(1);
-        }
-
-        // if FORKS available
-        while (pthread_mutex_trylock(&FORKS[PREVIOUS_FORK]) != 0 ||
-               pthread_mutex_trylock(&FORKS[NEXT_FORK]) != 0) {
-            pthread_mutex_unlock(&FORKS[PREVIOUS_FORK]);
-            pthread_mutex_unlock(&FORKS[NEXT_FORK]);
-            sleep(1);
-        }
-
-        ///////////    EatING     /////////////////////////////
-        printf("Philosopher : %d is EATING ...\n", PH_ID+1);
-        sleep(1);
-
-        // Put down FORKS
-        pthread_mutex_unlock(&FORKS[PREVIOUS_FORK]);
-        pthread_mutex_unlock(&FORKS[NEXT_FORK]);
-
-        // Put down bowl
-        pthread_mutex_unlock(&BOWLS[PH_ID]);
-    }
-}
+int FORKS[N];  // Array to represent the FORKS
 
 int main()
 {
-    pthread_t THREAD[N];
-    int ids[N];
+  int i;
 
-    // InitialIZE MUTEX ////////////////////
-    for (int i = 0; i < N; i++) {
-        pthread_mutex_init(&BOWLS[i], NULL);
-        pthread_mutex_init(&FORKS[i], NULL);
+  while (1) {
+    for (i = 0; i < N; i++) {
+      // Think
+      printf("Philosopher : %d is THINKING\n", i);
+      sleep(1);
+
+      // Acquire FORKS in a predetermined order
+      if (i % 2 == 0) {
+        // Pick left FORK
+        FORKS[i] = 1;
+        printf("Philosopher : %d picked up left FORK\n", i);
+        // Pick right FORK
+        FORKS[(i + 1) % N] = 1;
+        printf("Philosopher : %d picked up right FORK\n", i);
+      } else {
+        // Pick right FORK
+        FORKS[(i + 1) % N] = 1;
+        printf("Philosopher : %d picked up right FORK\n", i);
+        // Pick left FORK
+        FORKS[i] = 1;
+        printf("Philosopher : %d picked up left FORK\n", i);
+      }
+
+      // Eat
+      printf("Philosopher : %d is EATING\n", i);
+      sleep(1);
+      printf("Philosopher : %d stopped EATING\n", i);
+
+
+      // Put down FORKS
+      FORKS[i] = 0;
+      FORKS[(i + 1) % N] = 0;
     }
+  }
 
-    //          THREAD        ////
-    for (int i = 0; i < N; i++) {
-        ids[i] = ORDERING[i];
-        pthread_create(&THREAD[i], NULL, Philosopher_Function, &ids[i]);
-    }
-
-    
-    for (int i = 0; i < N; i++) {
-        pthread_join(THREAD[i], NULL);
-    }
-
-    return 0;
+////-----------THIS PROGRAM IS RUNNING BY ONLY ONE THREAD( MAIN THREAD)-----------------
+  return 0;
 }
